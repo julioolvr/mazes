829e7b12b45d0f5b2354b610cf380af8ced3a9fe
Julio <julio.olvr@gmail.com>
2019-10-29 09:30:42 -0300

    Generator algorithm selector

--- src/App.tsx
+++ src/App.tsx
@@ -5,28 +5,43 @@ import Maze from "Maze";
 import Grid from "lib/maze";
 import { binaryTree, sidewinder } from "lib/generators";
 
+const algorithms = [
+  { name: "Binary Tree", algo: binaryTree },
+  { name: "Sidewinder", algo: sidewinder }
+];
+
 const App: React.FC = () => {
   const maze = new Grid(20, 20);
-  binaryTree(maze);
+  const [algorithmIndex, setAlgorithmIndex] = useState(0);
+
+  algorithms[algorithmIndex].algo(maze);
 
   const mazeRef = useRef(maze);
   const [generatedAt, setGeneratedAt] = useState(new Date());
 
-  function generateBinaryTree() {
-    binaryTree(mazeRef.current);
-    setGeneratedAt(new Date());
-  }
-
-  function generateSidewinder() {
-    sidewinder(mazeRef.current);
+  function regenerate() {
+    algorithms[algorithmIndex].algo(mazeRef.current);
     setGeneratedAt(new Date());
   }
 
   return (
     <div>
+      Generators:
+      {algorithms.map((algorithm, i) => (
+        <label key={algorithm.name}>
+          <input
+            type="radio"
+            name="algorithm"
+            value={i}
+            checked={algorithmIndex === i}
+            onChange={e => setAlgorithmIndex(Number(e.target.value))}
+          />
+
+          {algorithm.name}
+        </label>
+      ))}
+      <button onClick={regenerate}>Regenerate</button>
       <Maze key={generatedAt.toISOString()} grid={mazeRef.current} />
-      <button onClick={generateBinaryTree}>Regenerate w/ Binary Tree</button>
-      <button onClick={generateSidewinder}>Regenerate w/ Sidewinder</button>
     </div>
   );
 };
